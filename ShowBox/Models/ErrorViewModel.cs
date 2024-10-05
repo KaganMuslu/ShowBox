da918068f9b5a355e15db657c40b88cab9f3fd74
Kağan Muslu <kaanwasder@gmail.com>
2024-10-05 20:57:43 +0300

    Add project files.

---
+++ ShowBox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShowBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
